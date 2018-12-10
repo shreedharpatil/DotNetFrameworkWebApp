ed4ead55bf5c2decdf37d1d6b0b0b239b70002c2
Shreedhar Patil <shreedhar.patil@honeywell.com>
2018-12-10 14:54:26 +0530

    intital commit

---
+++ SampleDotNetFrameworkApp/SampleDotNetFrameworkApp/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SampleDotNetFrameworkApp.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello, welcome to azure devops.. Dotnetframework...";
+        }
+    }
+}
